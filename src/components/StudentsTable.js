e290a08394c6a419fa85ee309bd3ac7b603341b0
Samuel Mata <Samuelab2@gmail.com>
2020-10-23 01:10:26 -0400

    minor refactoring

---
+++ src/components/StudentsTable.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Table } from 'reactstrap'
+import { studentsDB } from '../db'
+
+const StudentsTable = () => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Full Name</th>
+          <th>Email </th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>Major</th>
+          <th>Semester of Anticipaped Enrollment</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          studentsDB.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{index}</th>
+                <td>{item.first_name}, {item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.address}, {item.address2}, {item.city}, {item.zip}</td>
+                <td>{item.intended_major}</td>
+                <td>{item.semester_enrollment}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </Table>
+)}
+
+export default StudentsTable;
